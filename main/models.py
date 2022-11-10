a8b163dcaf0dac0e5cc06d94f28fd885efa15735
Tsunoyaka <mousakun@gmail.com>
2022-11-10 20:18:13 +0600

    Adinai

---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=125)
+    desc = models.TextField(blank=True, null=True)
+    creared_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(
+        to='Category', 
+        on_delete=models.SET_NULL, 
+        null=True,
+        related_name='books')
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=125, primary_key=True)
